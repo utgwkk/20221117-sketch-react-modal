26eaa583b6103a59800669a76404a81df2b0078a
utgwkk <utagawakiki@gmail.com>
2022-11-17 12:30:00 +0900

    root

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootEl = document.getElementById('root')!
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  rootEl
 );
 root.render(
   <React.StrictMode>
